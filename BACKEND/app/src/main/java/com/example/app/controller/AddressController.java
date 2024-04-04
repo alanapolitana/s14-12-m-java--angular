183a66f9836ad2230a1c981950841f352102ad0c
rodis2003 <rodisenrique73@gmail.com>
2024-04-04 16:27:30 -0500

    feat(backend): add endpoints
    add endpoint to update an address, add endpoint to delete an address and add swagger documentation

--- BACKEND/app/src/main/java/com/example/app/controller/AddressController.java
+++ BACKEND/app/src/main/java/com/example/app/controller/AddressController.java
@@ -2,13 +2,14 @@ package com.example.app.controller;
 
 import com.example.app.dto.address.AddressDTO;
 import com.example.app.dto.address.AddressPublicDataDTO;
+import com.example.app.dto.address.AddressUpdateDataDTO;
 import com.example.app.service.AddressService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import io.swagger.v3.oas.annotations.security.SecurityRequirements;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -28,7 +29,8 @@ public class AddressController {
 
     private final AddressService addressService;
 
-    @Operation(summary = "allows to register an address")
+    @Operation(summary = "allows to register an address",
+            description = "you must send a valid request body ")
     @ApiResponses(value = {
             @ApiResponse(
                     responseCode = "201", description = "Address created successfully",
@@ -48,7 +50,8 @@ public class AddressController {
         return ResponseEntity.created(new URI("/addresses")).build();
     }
 
-    @Operation(summary = "allows to obtain the addresses of a user")
+    @Operation(summary = "allows to obtain the addresses of a user",
+            description = "you must add the id of the user")
     @ApiResponses(value = {
             @ApiResponse(
                     responseCode = "200", description = "Addresses found successfully",
@@ -62,7 +65,43 @@ public class AddressController {
     })
     @GetMapping("/{id}")
     public ResponseEntity<List<AddressPublicDataDTO>> findAllAddressByUserId(
-            @PathVariable Long id){
+            @PathVariable Long id) {
         return ResponseEntity.ok().body(addressService.findAllByUserId(id));
+    }
+
+    @Operation(summary = "allows to update the addresses of a user",
+            description = "you must add the id of the address to update, you must send a valid request body")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200", description = "Addresses updated successfully",
+                    content = {
+                            @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = AddressPublicDataDTO.class))
+                    }),
+            @ApiResponse(responseCode = "400", description = "Valid data required", content = {@Content}),
+            @ApiResponse(responseCode = "403", description = "Requires authentication/permission", content = {@Content}),
+            @ApiResponse(responseCode = "404", description = "Unregistered address", content = {@Content}),
+            @ApiResponse(responseCode = "500", description = "There is a database problem", content = {@Content})
+    })
+    @PutMapping("/{id}")
+    public ResponseEntity<AddressPublicDataDTO> updateAddress(@PathVariable Long id,
+                                                              @Parameter(
+                                                                      schema = @Schema(implementation = AddressUpdateDataDTO.class))
+                                                              @RequestBody @Valid AddressUpdateDataDTO addressUpdateDataDTO) {
+        return ResponseEntity.ok().body(addressService.updateAddress(id, addressUpdateDataDTO));
+    }
+
+    @Operation(summary = "allows to delete the addresses of a user",
+            description = "you must add the id of the address to delete")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Addresses deleted successfully",content = {@Content}),
+            @ApiResponse(responseCode = "403", description = "Requires authentication/permission", content = {@Content}),
+            @ApiResponse(responseCode = "404", description = "Unregistered address", content = {@Content}),
+            @ApiResponse(responseCode = "500", description = "There is a database problem", content = {@Content})
+    })
+    @DeleteMapping("/{id}")
+    public ResponseEntity deleteAddressById(@PathVariable Long id) {
+        addressService.deleteAddress(id);
+        return ResponseEntity.noContent().build();
     }
 }
