8968e963894a5d0c09d4d4934124a1dc583bac1c
Cuch0 <germanosimani@gmail.com>
2024-04-04 13:04:25 -0300

    Creacion rutas products, login, register y respectivos componentes

--- FRONTEND/FooDelivery/src/app/app.routes.ts
+++ FRONTEND/FooDelivery/src/app/app.routes.ts
@@ -1,7 +1,13 @@
 import { Routes } from '@angular/router';
 import { MainContentComponent } from './home/main-content/main-content.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { ProductsListComponent } from './products/products-list/products-list.component';
 
 export const routes: Routes = [
     {path: "home", component: MainContentComponent},
+    {path: "login", component: LoginComponent},
+    {path: "register", component: RegisterComponent},
+    {path: "products", component: ProductsListComponent},
     {path: "**", redirectTo: "home"}
 ];
