5a1cca0d8efacda5297c6e1721f0b99979cc6d5e
Cuch0 <germanosimani@gmail.com>
2024-04-04 12:53:08 -0300

    Creacion ruta home y componentes home

--- FRONTEND/FooDelivery/src/app/app.routes.ts
+++ FRONTEND/FooDelivery/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { MainContentComponent } from './home/main-content/main-content.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "home", component: MainContentComponent},
+    {path: "**", redirectTo: "home"}
+];
