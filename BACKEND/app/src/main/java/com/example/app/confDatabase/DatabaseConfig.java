4729c7f912278bd44872cac9b8140fbb6da51a16
Luciano-A1 <141652211+Luciano-A1@users.noreply.github.com>
2024-04-04 18:51:35 -0300

    Correciones
    ----

--- BACKEND/app/src/main/java/com/example/app/confDatabase/DatabaseConfig.java
+++ BACKEND/app/src/main/java/com/example/app/confDatabase/DatabaseConfig.java
@@ -1,26 +1,26 @@
-package com.example.app.confDatabase;
-
-import javax.sql.DataSource;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-
-@Configuration
-public class DatabaseConfig {
-
-    @Bean
-    public DataSource dataSource() {
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        dataSource.setUrl(System.getenv("DB_URL") + System.getenv("DB_NAME"));
-        dataSource.setUsername(System.getenv("DB_USERNAME"));
-        dataSource.setPassword(System.getenv("DB_PASSWORD"));
-        return dataSource;
-    }
-
-    @Bean
-    public DataSourceTransactionManager transactionManager() {
-        return new DataSourceTransactionManager(dataSource());
-    }
-}
+//package com.example.app.confDatabase;
+//
+//import javax.sql.DataSource;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+//import org.springframework.jdbc.datasource.DriverManagerDataSource;
+//
+//@Configuration
+//public class DatabaseConfig {
+//
+//    @Bean
+//    public DataSource dataSource() {
+//        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+//        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+//        dataSource.setUrl(System.getenv("DB_URL") + System.getenv("DB_NAME"));
+//        dataSource.setUsername(System.getenv("DB_USERNAME"));
+//        dataSource.setPassword(System.getenv("DB_PASSWORD"));
+//        return dataSource;
+//    }
+//
+//    @Bean
+//    public DataSourceTransactionManager transactionManager() {
+//        return new DataSourceTransactionManager(dataSource());
+//    }
+//}
