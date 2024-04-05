53d42aea1d551f7fd063f46c78b72cdcd7c0aec3
Hiram Chavez <virtual.liga@gmail.com>
2024-04-05 12:42:27 -0600

    refactor/modify data for signup

--- BACKEND/app/src/main/java/com/example/app/dto/user/UserToSignUpDto.java
+++ BACKEND/app/src/main/java/com/example/app/dto/user/UserToSignUpDto.java
@@ -14,19 +14,23 @@ import java.io.Serializable;
 public record UserToSignUpDto(
   @NotBlank(message = "El nombre completo es requerido")
   @Pattern(regexp = "^[a-zA-Z]+(\\s[a-zA-Z]+)+$", message = "El nombre completo debe contener solo letras y espacios, y debe incluir un nombre y un apellido")
+  @Size(min = 3, message = "El nombre completo debe tener al menos 3 caracteres")
+  @Size(max = 30, message = "El nombre completo debe tener como máximo 30 caracteres")
   String fullName,
   @NotBlank(message = "El numero de teléfono es requerido")
-  @Pattern(regexp = "^[0-9]{10}$", message = "El número de teléfono deben ser dígitos y de longitud 10")
+  @Pattern(regexp = "^[0-9]{10,15}$", message = "El número de teléfono deben ser dígitos y longitud entre 10-15")
   String phone,
   @NotBlank(message = "El email es requerido")
-    @Pattern(regexp = "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,6}+$", message = "El email no es válido")
+  @Pattern(regexp = "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,6}+$", message = "El email no es válido")
   String email,
   @NotBlank(message = "La contraseña es requerida")
   @Size(min = 8, message = "La contraseña debe tener al menos 8 caracteres")
   @Size(max = 16, message = "La contraseña debe tener como máximo 16 caracteres")
   String password,
 
-  @JsonIgnore
+  @Size(min = 3, message = "El alias debe tener al menos 3 caracteres")
+  @Size(max = 30, message = "El alias debe tener como máximo 30 caracteres")
+  @Pattern(regexp = "^$|^\\w+$", message = "El username debe estar vacío o contener solo caracteres alfanuméricos y _")
   String alias,
 
   @JsonIgnore
