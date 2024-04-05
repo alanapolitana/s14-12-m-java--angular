08d50c374fedc4d6db6801fe5f37e76952904c82
rodis2003 <rodisenrique73@gmail.com>
2024-04-05 11:14:31 -0500

    feat(backend): add max size

--- BACKEND/app/src/main/java/com/example/app/dto/address/AddressDTO.java
+++ BACKEND/app/src/main/java/com/example/app/dto/address/AddressDTO.java
@@ -3,6 +3,7 @@ package com.example.app.dto.address;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 import java.io.Serializable;
 
@@ -16,14 +17,18 @@ public record AddressDTO(
         UserToSaveAddress user,
 
         @NotBlank(message = "La provincia es requerida")
+        @Size(max = 30, message = "La provincia debe contener menos de 30 caracteres")
         String province,
 
         @NotBlank(message = "La localidad es requerida")
+        @Size(max = 50, message = "La localidad debe contener menos de 50 caracteres")
         String locality,
 
         @NotBlank(message = "El numero de su recidencia es requerido")
+        @Size(max = 70, message = "La direccion debe contener menos de 70 caracteres")
         String streetNumber,
 
+        @Size(max = 150, message = "El punto de referencia debe contener menos de 150 caracteres")
         String referencePoint
 
 ) implements Serializable {
--- BACKEND/app/src/main/java/com/example/app/dto/address/AddressUpdateDataDTO.java
+++ BACKEND/app/src/main/java/com/example/app/dto/address/AddressUpdateDataDTO.java
@@ -1,14 +1,17 @@
 package com.example.app.dto.address;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 import java.io.Serializable;
 
 public record AddressUpdateDataDTO(
 
         @NotBlank(message = "El numero de su recidencia es requerido")
+        @Size(max = 70, message = "La direccion debe contener menos de 70 caracteres")
         String streetNumber,
 
+        @Size(max = 150, message = "El punto de referencia debe contener menos de 150 caracteres")
         String referencePoint
 
 ) implements Serializable {
