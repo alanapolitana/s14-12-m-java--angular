02da8db0111a238c0c99853006f5d9f3f11ee6cb
marcovirinni@gmail.com <marcovirinni@gmail.com>
2024-04-07 14:42:24 -0300

    run

--- FRONTEND/FooDelivery/src/app/app.component.ts
+++ FRONTEND/FooDelivery/src/app/app.component.ts
@@ -1,11 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-<<<<<<< HEAD
 import { HttpClient } from '@angular/common/http';
 
 
-=======
->>>>>>> 5bdf00885c09b64f9ecff4cd81294b1a645f33d3
 
 @Component({
   selector: 'app-root',
