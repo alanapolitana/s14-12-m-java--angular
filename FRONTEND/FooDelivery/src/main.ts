88068131efff9f67467cb2618403010b7a46aed7
marcovirinni@gmail.com <marcovirinni@gmail.com>
2024-04-07 14:46:46 -0300

    clean

--- FRONTEND/FooDelivery/src/main.ts
+++ FRONTEND/FooDelivery/src/main.ts
@@ -1,21 +1,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-import { NgModel } from '@angular/forms';
-import { NgModule } from '@angular/core';
->>>>>>> 890007c4639c9be19da08a22b928a5d2c8ff2d2a
+
+
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
 
-<<<<<<< HEAD
-=======
-=======
-
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
->>>>>>> 5bdf00885c09b64f9ecff4cd81294b1a645f33d3
->>>>>>> 890007c4639c9be19da08a22b928a5d2c8ff2d2a
